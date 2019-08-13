905012d8754b781cb89f38241a5091f8a993ff1c
Marcelo Rezin <marcelo.rezin@gmail.com>
2019-08-13 18:45:58 -0300

    Ajuste de calculo e descrições

--- app/src/main/java/com/util/utilitarios/ConversaoPendriveDisqueteActivity.java
+++ app/src/main/java/com/util/utilitarios/ConversaoPendriveDisqueteActivity.java
@@ -10,11 +10,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class ConversaoPendriveDisqueteActivity extends AppCompatActivity  {
+    private static final BigDecimal UM_GB_EM_MB = new BigDecimal("1024");
+    private static final BigDecimal TAMANHO_DISQUETE_EM_MB = new BigDecimal("1.44");
+
     private EditText editPenDriveValor;
     private Button btnCalcular;
-    private TextView resultado;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,24 +25,24 @@ public class ConversaoPendriveDisqueteActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_conversao_pendrive_disquete);
 
         editPenDriveValor = findViewById(R.id.edit_penDrive);
-        resultado= findViewById(R.id.txt_ResultadoPendrive);
 
         btnCalcular= findViewById(R.id.btn_penDrive);
         btnCalcular.setOnClickListener(o->{
             try {
-                String pendriveValorString= editPenDriveValor.getText().toString();
+                String pendriveValorString = editPenDriveValor.getText().toString();
                 if(pendriveValorString.isEmpty()){
-                    throw new Exception("Informe Tamanho do Pen-Drive!");
+                    throw new Exception("Informe o tamanho do pendrive!");
                 }
+
                 BigDecimal pendriveValor = new BigDecimal(pendriveValorString);
-                /*if(pendriveValor.compareTO(new BigDecimal(0.0))==0){
-                    throw new Exception("A Tamanho do pendrive esta errado!");
-                }   Ver Marcelo porque não executou a instrução    */
+                if(pendriveValor.compareTo(new BigDecimal("0")) == 0){
+                    throw new Exception("Tamanho do pendrive deve ser maior que 0!");
+                }
 
-                BigDecimal pendriveMega= pendriveValor.multiply(new BigDecimal("1024"));
-                BigDecimal resultadoDisk=pendriveMega.divide(new BigDecimal("1.44"));
-                resultado.setText("Um Pen-drive de: " + pendriveValorString+" é o equivalente a: " + resultadoDisk + " Disquetes");
+                BigDecimal pendriveMega = pendriveValor.multiply(UM_GB_EM_MB);
+                BigDecimal resultadoDisk = pendriveMega.divide(TAMANHO_DISQUETE_EM_MB, 2, RoundingMode.HALF_DOWN);
 
+                Toast.makeText(ConversaoPendriveDisqueteActivity.this, "Um pendrive de " + pendriveValorString +"GB equivale a " + resultadoDisk + " disquete(s)", Toast.LENGTH_LONG).show();
             }catch (Exception e){
                 Toast.makeText(ConversaoPendriveDisqueteActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
             }
